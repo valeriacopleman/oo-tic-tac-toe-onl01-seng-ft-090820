1d0b6eae43fae7ebdc3eb5dde8ac3e50da132fba
Valeria  Copleman <valeriacopleman@gmail.com>
2020-09-20 23:26:51 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -4,4 +4,14 @@ class TicTacToe
     @board = Array.new(9, " ")
   end
   
+  WIN_COMBINATIONS =
+  [[1,2,3],
+  [4,5,6],
+  [7,8,9],
+  [1,4,7],
+  [2,5,8],
+  [3,6,9],
+  [1,5,9],
+  [3,5,7]]
+  
 end
