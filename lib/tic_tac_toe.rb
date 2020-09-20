52dc8568bdd1cc13471b0b5bb2dcedad764768f5
Valeria  Copleman <valeriacopleman@gmail.com>
2020-09-20 23:32:06 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -15,7 +15,11 @@ class TicTacToe
   [0,4,8],]
   
   def display_board
-    @board = board
+    puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
+    puts "-----------"
+    puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
+    puts "-----------"
+    puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
   end
   
 end
